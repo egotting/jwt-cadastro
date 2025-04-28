98973a75ee00f9c12d42000d2c336d61fff20aab
egotting <henricodss@gmail.com>
2025-04-28 16:33:19 -0300

    feat: Implementando design pattern Builder e refatorando para deixar melhor visualmente

--- src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Entities/User/User.java
+++ src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Entities/User/User.java
@@ -1,24 +1,20 @@
 package br.com.egotting.simple_api_restful_springboot.domain.Entities.User;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
-import br.com.egotting.simple_api_restful_springboot.domain.Enums.Roles;
+import br.com.egotting.simple_api_restful_springboot.domain.Entities.Role.Role;
+import br.com.egotting.simple_api_restful_springboot.domain.Enums.NameRoles;
 import jakarta.persistence.*;
 
 import br.com.egotting.simple_api_restful_springboot.Validations.Interface.IPasswordValidator;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 
 @Entity
 @Table(name = "tb_users")
-public class User implements UserDetails {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "user_id")
@@ -31,62 +27,41 @@ public class User implements UserDetails {
     @Column(name = "user_password")
     private String password;
     @Column(name = "user_roles")
-    @Enumerated(EnumType.STRING)
-    private Roles roles;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    @JoinTable(name = "tb_users_tb_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles;
     @Column(name = "user_created_account")
-    private LocalDateTime createdAccount = LocalDateTime.now();
+    private final LocalDateTime createdAccount = LocalDateTime.now();
 
     public User() {
     }
 
-    public User(String email, String password) {
-        this.email = email;
-        this.password = password;
+    public User(builder builder) {
+        this.email = builder.email;
+        this.password = builder.password;
+        this.roles = new ArrayList<>();
+
+        Role defaultRole = new Role.builder().roles(NameRoles.valueOf("USER")).build();
+        this.roles.add(defaultRole);
     }
 
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        if (this.roles == Roles.ADMIN)
-            return List.of(new SimpleGrantedAuthority("ADMIN"), new SimpleGrantedAuthority(("USER")));
-        else return List.of(new SimpleGrantedAuthority("USER"));
-    }
-
-    @Override
-    public String getUsername() {
-        return getEmail();
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && roles == user.roles && Objects.equals(createdAccount, user.createdAccount);
+        return Objects.equals(id, user.id) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(roles, user.roles) && Objects.equals(createdAccount, user.createdAccount);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(id, email, password, roles, createdAccount);
+    }
+
+    public List<Role> getRoles() {
+        return roles;
     }
 
     public String getEmail() {
@@ -97,28 +72,37 @@ public class User implements UserDetails {
         this.email = email;
     }
 
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public Roles getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Roles roles) {
-        this.roles = roles;
+    public String getPassword() {
+        return password;
     }
 
     public LocalDateTime getCreatedAccount() {
         return createdAccount;
     }
 
-    public void setCreatedAccount(LocalDateTime createdAccount) {
-        this.createdAccount = createdAccount;
+    public static class builder {
+
+        private String email;
+        private String password;
+
+        public builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+
+        public User build() {
+            return new User(this);
+        }
     }
+
 }
