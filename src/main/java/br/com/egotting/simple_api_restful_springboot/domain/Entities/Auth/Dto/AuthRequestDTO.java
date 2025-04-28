35d8f374cedeca3cd0785c9d6cc61ad1252137b6
egotting <henricodss@gmail.com>
2025-04-28 16:16:51 -0300

    Fix: Trocando o tipo para record

--- src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Entities/Auth/Dto/AuthRequestDTO.java
+++ src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Entities/Auth/Dto/AuthRequestDTO.java
@@ -1,65 +1,11 @@
 package br.com.egotting.simple_api_restful_springboot.domain.Entities.Auth.Dto;
 
 import br.com.egotting.simple_api_restful_springboot.Validations.Interface.IPasswordValidator;
-import br.com.egotting.simple_api_restful_springboot.domain.Enums.Roles;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-import java.util.Objects;
-
-public class AuthRequestDTO {
-    @Email
-    @NotBlank(message = "Email não pode estar vazio")
-    String Email;
-    @Size(min = 8)
-    @IPasswordValidator
-    @NotBlank(message = "Email não pode estar vazio")
-    String Password;
-    Roles roles;
-
-    public AuthRequestDTO() {
-    }
-
-    public AuthRequestDTO(String email, String password) {
-        Email = email;
-        Password = password;
-        this.roles = Roles.USER;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        AuthRequestDTO that = (AuthRequestDTO) o;
-        return Objects.equals(Email, that.Email) && Objects.equals(Password, that.Password) && roles == that.roles;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(Email, Password, roles);
-    }
-
-    public String getEmail() {
-        return Email;
-    }
-
-    public void setEmail(String email) {
-        Email = email;
-    }
-
-    public String getPassword() {
-        return Password;
-    }
-
-    public void setPassword(String password) {
-        Password = password;
-    }
-
-    public Roles getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Roles roles) {
-        this.roles = roles;
-    }
+public record AuthRequestDTO(@Email @NotBlank(message = "Email não pode estar vazio") String email,
+                             @Size(min = 8) @IPasswordValidator @NotBlank(message = "Senha não pode estar vazio") String password) {
 }
+
