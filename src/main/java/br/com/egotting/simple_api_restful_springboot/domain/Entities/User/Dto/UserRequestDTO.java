6c62da0603f9ca0b4bf7036bef828b3b248c16d0
egotting <henricodss@gmail.com>
2025-04-28 16:34:51 -0300

    .

--- src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Entities/User/Dto/UserRequestDTO.java
+++ src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Entities/User/Dto/UserRequestDTO.java
@@ -1,7 +1,6 @@
 package br.com.egotting.simple_api_restful_springboot.domain.Entities.User.Dto;
 
 import br.com.egotting.simple_api_restful_springboot.Validations.Interface.IPasswordValidator;
-import br.com.egotting.simple_api_restful_springboot.domain.Enums.Roles;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
--- src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Entities/User/Dto/UserResponseDTO.java
+++ src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Entities/User/Dto/UserResponseDTO.java
@@ -1,30 +1,7 @@
 package br.com.egotting.simple_api_restful_springboot.domain.Entities.User.Dto;
 
-import br.com.egotting.simple_api_restful_springboot.domain.Enums.Roles;
 
+public record UserResponseDTO(String email,
+                              java.util.List<br.com.egotting.simple_api_restful_springboot.domain.Entities.Role.Role> roles) {
 
-public class UserResponseDTO {
-    String email;
-    Roles Roles;
-
-    public UserResponseDTO(String email, Roles roles) {
-        this.email = email;
-        this.Roles = roles;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Roles getRoles() {
-        return Roles;
-    }
-
-    public void setRoles(Roles roles) {
-        Roles = roles;
-    }
 }
