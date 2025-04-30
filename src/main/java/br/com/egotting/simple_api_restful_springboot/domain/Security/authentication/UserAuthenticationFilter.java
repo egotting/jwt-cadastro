fb0658542b056ae77a674298e0f8633c8150e214
egotting <henricodss@gmail.com>
2025-04-30 18:59:46 -0300

    refactor: alterando a forma de implementaçao

--- src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Security/authentication/UserAuthenticationFilter.java
+++ src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Security/authentication/UserAuthenticationFilter.java
@@ -1,63 +1,58 @@
 package br.com.egotting.simple_api_restful_springboot.domain.Security.authentication;
 
-import br.com.egotting.simple_api_restful_springboot.api.Routes.Routes;
 import br.com.egotting.simple_api_restful_springboot.domain.Entities.User.User;
 import br.com.egotting.simple_api_restful_springboot.domain.Repositories.User.IUserRepository;
-import br.com.egotting.simple_api_restful_springboot.domain.Security.config.SecurityConfiguration;
 import br.com.egotting.simple_api_restful_springboot.domain.Security.userdetails.UserDetailsImpl;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 @Component
 public class UserAuthenticationFilter extends OncePerRequestFilter {
 
-    @Autowired
-    private IUserRepository userRepository;
-    @Autowired
-    private JwtTokenService jwtTokenService;
+    private final IUserRepository userRepository;
+    private final JwtTokenService jwtTokenService;
+
+    public UserAuthenticationFilter(IUserRepository userRepository, JwtTokenService jwtTokenService) {
+        this.userRepository = userRepository;
+        this.jwtTokenService = jwtTokenService;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        if (checkIfEndpointIsNotPublic(request)) {
-            String token = recoveryToken(request);
+        try {
+            String token = recoverToken(request);
             if (token != null) {
                 String subject = jwtTokenService.getSubjectToken(token);
-                User user = userRepository.findByEmail(subject).get();
+                User user = userRepository.findByEmail(subject);
                 UserDetailsImpl userDetails = new UserDetailsImpl(user);
 
                 Authentication auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 
                 SecurityContextHolder.getContext().setAuthentication(auth);
-
-            } else {
-                throw new RuntimeException("O token esta ausente.");
             }
+        } catch (Exception e) {
+            throw new HttpServerErrorException(HttpStatus.UNAUTHORIZED, "Unauthorized");
         }
+        filterChain.doFilter(request, response);
+
     }
 
-    private String recoveryToken(HttpServletRequest request) {
-        String authorizationHeader = request.getHeader("Authorization");
-        if (authorizationHeader != null) {
-            return authorizationHeader.replace("Bearer ", "");
-        }
-        return null;
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
     }
 
-    private boolean checkIfEndpointIsNotPublic(HttpServletRequest request) {
-        String requestURI = request.getRequestURI();
-        return !Arrays.asList(Routes.ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED).contains(requestURI);
-    }
 }
