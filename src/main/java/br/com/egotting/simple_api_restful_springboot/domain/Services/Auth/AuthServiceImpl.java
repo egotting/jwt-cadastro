8e830e125b2b2f44e7ce4ad11b7ca861ab681730
egotting <henricodss@gmail.com>
2025-04-30 18:24:29 -0300

    Refactor: Refatorando as injeçao de dependencias e implementano para o usuario ser sempre cargo COMMON

--- src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Services/Auth/AuthServiceImpl.java
+++ src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Services/Auth/AuthServiceImpl.java
@@ -3,19 +3,17 @@ package br.com.egotting.simple_api_restful_springboot.domain.Services.Auth;
 import br.com.egotting.simple_api_restful_springboot.Pattern.ResultPattern.Error;
 import br.com.egotting.simple_api_restful_springboot.Pattern.ResultPattern.Result;
 import br.com.egotting.simple_api_restful_springboot.domain.Entities.Auth.Dto.AuthRequestDTO;
-import br.com.egotting.simple_api_restful_springboot.domain.Entities.Role.Role;
 import br.com.egotting.simple_api_restful_springboot.domain.Entities.User.Dto.CreateUserRequestDTO;
 import br.com.egotting.simple_api_restful_springboot.domain.Entities.User.User;
-import br.com.egotting.simple_api_restful_springboot.domain.Enums.ResponseStatus;
+import br.com.egotting.simple_api_restful_springboot.domain.Enums.NameRoles;
+import br.com.egotting.simple_api_restful_springboot.domain.Repositories.Role.IRoleRepository;
 import br.com.egotting.simple_api_restful_springboot.domain.Repositories.User.IUserRepository;
 import br.com.egotting.simple_api_restful_springboot.domain.Security.authentication.JwtTokenService;
 import br.com.egotting.simple_api_restful_springboot.domain.Security.config.SecurityConfiguration;
 import br.com.egotting.simple_api_restful_springboot.domain.Security.userdetails.UserDetailsImpl;
 import br.com.egotting.simple_api_restful_springboot.domain.Services.Auth.Interface.IAuthService;
-import br.com.egotting.simple_api_restful_springboot.domain.Services.User.UserServicesImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -25,22 +23,25 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-import static br.com.egotting.simple_api_restful_springboot.Pattern.ResultPattern.Error.*;
 
 @Service
 public class AuthServiceImpl implements IAuthService {
-    private static final Logger log = LoggerFactory.getLogger(UserServicesImpl.class);
+    private static final Logger log = LoggerFactory.getLogger(AuthServiceImpl.class);
 
-    @Autowired
-    private IUserRepository userRepository;
+    private final IUserRepository userRepository;
+    private final IRoleRepository roleRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenService jwtTokenService;
+    private final SecurityConfiguration securityConfiguration;
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
-    @Autowired
-    private JwtTokenService jwtTokenService;
-    @Autowired
-    private SecurityConfiguration securityConfiguration;
-
+    public AuthServiceImpl(IUserRepository userRepository, IRoleRepository roleRepository, AuthenticationManager authenticationManager,
+                           JwtTokenService jwtTokenService, SecurityConfiguration securityConfiguration) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenService = jwtTokenService;
+        this.securityConfiguration = securityConfiguration;
+    }
 
     @Override
     public ResponseEntity<Result<?>> login(AuthRequestDTO data) {
@@ -51,6 +52,7 @@ public class AuthServiceImpl implements IAuthService {
             Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
             UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
             jwtTokenService.GenerateToken(userDetails);
+
             return ResponseEntity
                     .status(HttpStatus.OK)
                     .body(Result.Success(Error.Success("Usuario.Logado", "Usuario Logado com Sucesso")));
@@ -65,9 +67,18 @@ public class AuthServiceImpl implements IAuthService {
     @Override
     public ResponseEntity<Result<?>> register(CreateUserRequestDTO data) {
         try {
+            var findRole = roleRepository.findByNameRoles(NameRoles.COMMON).orElseThrow(() ->
+                    new RuntimeException("Role not found"));
+
+            if (userRepository.existsByEmail(data.email())) {
+                return ResponseEntity
+                        .status(HttpStatus.BAD_REQUEST)
+                        .body(Result.Failure(Error.Warning("User.Existis", "Usuario ja existe")));
+            }
             User newUser = new User.builder()
                     .email(data.email())
                     .password(securityConfiguration.passwordEncoder().encode(data.password()))
+                    .role(findRole)
                     .build();
 
             userRepository.save(newUser);
