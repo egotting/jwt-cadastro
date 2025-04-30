e236b68dbc758eb41aff300d53bca24bba0a08b4
egotting <henricodss@gmail.com>
2025-04-30 18:22:22 -0300

    .

--- src/main/java/br/com/egotting/simple_api_restful_springboot/api/User/AuthController.java
+++ src/main/java/br/com/egotting/simple_api_restful_springboot/api/User/AuthController.java
@@ -2,25 +2,25 @@ package br.com.egotting.simple_api_restful_springboot.api.User;
 
 import br.com.egotting.simple_api_restful_springboot.Pattern.ResultPattern.Result;
 import br.com.egotting.simple_api_restful_springboot.domain.Entities.User.Dto.CreateUserRequestDTO;
-import br.com.egotting.simple_api_restful_springboot.domain.Services.Auth.Interface.IAuthService;
-import org.springframework.web.bind.annotation.RequestMapping;
+import br.com.egotting.simple_api_restful_springboot.domain.Services.Auth.AuthServiceImpl;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.egotting.simple_api_restful_springboot.domain.Entities.Auth.Dto.AuthRequestDTO;
-import br.com.egotting.simple_api_restful_springboot.domain.Entities.GeneralDTOs.GeneralRequestDTO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-@RequestMapping("/v1/api")
 public class AuthController {
 
-    @Autowired
-    IAuthService services;
+
+    private final AuthServiceImpl services;
+
+    public AuthController(AuthServiceImpl service) {
+        this.services = service;
+    }
 
     @PostMapping("/login")
     public ResponseEntity<Result<?>> login(
