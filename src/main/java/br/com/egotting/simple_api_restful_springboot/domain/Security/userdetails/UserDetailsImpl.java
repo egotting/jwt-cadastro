b66057d8c9cc06337aff2baea526c82351f8d28a
egotting <henricodss@gmail.com>
2025-04-30 19:00:15 -0300

    refactor: refatorandoa forma de encontrar o tipo de role do usuario

--- src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Security/userdetails/UserDetailsImpl.java
+++ src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Security/userdetails/UserDetailsImpl.java
@@ -1,31 +1,29 @@
 package br.com.egotting.simple_api_restful_springboot.domain.Security.userdetails;
 
 import br.com.egotting.simple_api_restful_springboot.domain.Entities.User.User;
+import br.com.egotting.simple_api_restful_springboot.domain.Enums.NameRoles;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.stream.Collectors;
+import java.util.List;
 
 public class UserDetailsImpl implements UserDetails {
 
-    User user;
+
+    private final User user;
 
     public UserDetailsImpl(User user) {
         this.user = user;
     }
 
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return user.getRoles()
-                .stream()
-                .map(role ->
-                        new SimpleGrantedAuthority(
-                                role.getNameRoles()
-                                        .name()))
-                .collect(Collectors.toList());
+        if (user.getRoles().getNameRoles() == NameRoles.ADMIN)
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_COMMON"));
+        else
+            return List.of(new SimpleGrantedAuthority("ROLE_COMMON"));
     }
 
     @Override
