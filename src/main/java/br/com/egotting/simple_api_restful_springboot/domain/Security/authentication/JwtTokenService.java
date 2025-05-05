bb5e6d08e15fad2ceb8e25a9eb3848ee5536d278
egotting <henricodss@gmail.com>
2025-05-05 18:00:02 -0300

    refactor: Refatorei o token service

--- src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Security/authentication/JwtTokenService.java
+++ src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Security/authentication/JwtTokenService.java
@@ -1,50 +1,46 @@
 package br.com.egotting.simple_api_restful_springboot.domain.Security.authentication;
 
+import br.com.egotting.simple_api_restful_springboot.domain.Entities.User.User;
 import br.com.egotting.simple_api_restful_springboot.domain.Security.userdetails.UserDetailsImpl;
 import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTCreationException;
-import com.auth0.jwt.exceptions.JWTVerificationException;
-import org.springframework.beans.factory.annotation.Value;
+import com.nimbusds.jose.jwk.JWKException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
 import java.time.*;
+import java.util.stream.Collectors;
 
 @Service
 public class JwtTokenService {
     private static final String ISSUER = "simple-api-restul-springboot";
-    @Value("${jwt.private.key}")
-    private String SECRET_KEY;
+    private JwtEncoder jwtEncoder;
+
+    public JwtTokenService(JwtEncoder jwtEncoder) {
+        this.jwtEncoder = jwtEncoder;
+    }
+
 
     public String GenerateToken(UserDetailsImpl user) {
 
-        try {
-            Algorithm algoritm = Algorithm.HMAC256(SECRET_KEY);
-            String token = JWT.create()
-                    .withIssuer(ISSUER)
-                    .withIssuedAt(creationAt())
-                    .withExpiresAt(expireAt())
-                    .withSubject(user.getUsername())
-                    .sign(algoritm);
-            System.out.println("Token: " + token);
-            return token;
-        } catch (JWTCreationException e) {
-            throw new JWTCreationException("Erro ao gerar o token", e);
-        }
+        var scopes = user.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        var claims = JwtClaimsSet.builder()
+                .issuer(ISSUER)
+                .issuedAt(creationAt())
+                .expiresAt(expireAt())
+                .subject(user.getUsername())
+                .claim("authorities", scopes)
+                .build();
+        System.out.println("Token gerado: " + jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue());
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
     }
 
-    public String getSubjectToken(String token) {
-        try {
-            Algorithm algoritm = Algorithm.HMAC256(SECRET_KEY);
-            return JWT.require(algoritm)
-                    .withIssuer(ISSUER)
-                    .build()
-                    .verify(token)
-                    .getSubject();
-        } catch (JWTVerificationException e) {
-            throw new JWTVerificationException("Token invalido", e);
-        }
-    }
 
     public Instant creationAt() {
         return LocalDateTime.now().toInstant(ZoneOffset.of("-03:00"));
