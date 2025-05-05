5fda64d756fdc309ce76a72878532cc48e0d1d60
egotting <henricodss@gmail.com>
2025-05-05 18:00:48 -0300

    feat: Implementaçao de jwtEncoder e Decoder e oauth2

--- src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Security/config/SecurityConfiguration.java
+++ src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Security/config/SecurityConfiguration.java
@@ -2,60 +2,75 @@ package br.com.egotting.simple_api_restful_springboot.domain.Security.config;
 
 import br.com.egotting.simple_api_restful_springboot.api.Routes.Routes;
 import br.com.egotting.simple_api_restful_springboot.domain.Enums.NameRoles;
-import br.com.egotting.simple_api_restful_springboot.domain.Security.authentication.UserAuthenticationFilter;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import java.util.Arrays;
+import java.security.interfaces.RSAPrivateCrtKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
-    private final UserAuthenticationFilter filter;
+    // chamar token privado e publico
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey privateKey;
 
-    public SecurityConfiguration(UserAuthenticationFilter filter) {
-        this.filter = filter;
-    }
+    @Value("${jwt.public.key}")
+    private RSAPublicKey publicKey;
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, Routes.ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED).permitAll()
+                        .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
-                .formLogin(form -> form.disable())
-//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.GET, "/api/v1/user/listar").hasRole(NameRoles.ADMIN.name())
-                        .requestMatchers(HttpMethod.DELETE, "/api/v1/user/deletar/**").hasRole(NameRoles.ADMIN.name())
-                        .requestMatchers(HttpMethod.PUT, "/api/v1/user/atualizar/**").authenticated()
-                        .requestMatchers(HttpMethod.GET, "/api/v1/user/encontrar/**").authenticated()
-                        .requestMatchers(HttpMethod.POST, Routes.ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED).permitAll()
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
+    // decoder
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConf) throws Exception {
-        return authenticationConf.getAuthenticationManager();
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
     }
+
+    //encoder
+    @Bean
+    JwtEncoder jwtEncoder() {
+        var jwk = new RSAKey.Builder(publicKey).privateKey(privateKey).build();
+        var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 
 }
