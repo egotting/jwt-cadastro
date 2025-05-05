a3dd93156a198af55fe1bdb3fcbb0f2e36a278a8
egotting <henricodss@gmail.com>
2025-05-05 17:58:24 -0300

    .

--- src/main/java/br/com/egotting/simple_api_restful_springboot/api/User/AuthController.java
+++ src/main/java/br/com/egotting/simple_api_restful_springboot/api/User/AuthController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.egotting.simple_api_restful_springboot.domain.Entities.Auth.Dto.AuthRequestDTO;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
