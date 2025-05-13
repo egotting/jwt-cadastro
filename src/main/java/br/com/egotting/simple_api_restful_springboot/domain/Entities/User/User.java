8adeaca05c81a20afb491cfb7164302e47dad659
egotting <henricodss@gmail.com>
2025-05-13 18:28:09 -0300

    Refactor: refatorando metodos de alteração de usuario

--- src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Entities/User/User.java
+++ src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Entities/User/User.java
@@ -1,14 +1,12 @@
 package br.com.egotting.simple_api_restful_springboot.domain.Entities.User;
 
 import java.time.LocalDateTime;
-import java.util.*;
 
 import br.com.egotting.simple_api_restful_springboot.domain.Entities.Role.Role;
 import jakarta.persistence.*;
 
 import br.com.egotting.simple_api_restful_springboot.Validations.Interface.IPasswordValidator;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Size;
 
 
 @Entity
@@ -21,13 +19,13 @@ public class User {
     @Email
     @Column(name = "user_email")
     private String email;
-    @Size(min = 8)
     @IPasswordValidator
     @Column(name = "user_password")
     private String password;
     @ManyToOne
     @JoinColumn(name = "role_user", referencedColumnName = "name_roles")
     private Role roles;
+
     @Column(name = "user_created_account")
     private final LocalDateTime createdAccount = LocalDateTime.now();
 
@@ -41,33 +39,21 @@ public class User {
     }
 
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(roles, user.roles) && Objects.equals(createdAccount, user.createdAccount);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, email, password, roles, createdAccount);
-    }
-
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
+    public void changeEmail(String email) {
         this.email = email;
     }
 
-    public void setPassword(String password) {
+    public void changePassword(String password) {
         this.password = password;
     }
 
+
     public String getPassword() {
         return password;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public Role getRoles() {
