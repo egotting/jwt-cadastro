ef2abfe43a5d520f72d3683f15ec0687331fc11e
egotting <henricodss@gmail.com>
2025-05-13 18:27:32 -0300

    .

--- src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Entities/User/Dto/UpdateRequestDTO.java
+++ src/main/java/br/com/egotting/simple_api_restful_springboot/domain/Entities/User/Dto/UpdateRequestDTO.java
@@ -1,19 +1,12 @@
 package br.com.egotting.simple_api_restful_springboot.domain.Entities.User.Dto;
 
-import br.com.egotting.simple_api_restful_springboot.Validations.Interface.IPasswordValidator;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 public class UpdateRequestDTO {
 
-    @Email
     private String newEmail;
-    @Size(min = 8)
-    @IPasswordValidator
     private String newPassword;
     @NotBlank(message = "Senha não pode estar vazio")
     private String oldPassword;
@@ -39,19 +32,6 @@ public class UpdateRequestDTO {
 
     public LocalDateTime getUpdateAccount() {
         return updateAccount;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(newEmail, newPassword, updateAccount);
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        UpdateRequestDTO that = (UpdateRequestDTO) o;
-        return Objects.equals(newEmail, that.newEmail) && Objects.equals(newPassword, that.newPassword) && Objects.equals(updateAccount, that.updateAccount);
     }
 
 
